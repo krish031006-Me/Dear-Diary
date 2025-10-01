93c94a95e86229c7babd1979551743aa6fcfb58d
Krish Bhardwaj <180808748+krish031006-Me@users.noreply.github.com>
2025-10-01 19:54:30 +0530

    Removed the fade-in effect that didn't work

--- static/script.js
+++ static/script.js
@@ -7,12 +7,6 @@ document.addEventListener("DOMContentLoaded", function(event){
 
     // Calling the function only if there is a title id element present i.e only on dashboard
     if(document.getElementById("title")){
-        // This is the event listener for my custom event
-        const hr = document.querySelector("hr");
-        hr.addEventListener("afterType", () => {
-            // classList is a property of DOM that let's you add classes to your js
-            hr.classList.add("fade-in"); 
-        });
         // Calling the type_title() function
         title_type();
     } 
@@ -53,12 +47,6 @@ function title_type(){
         // an escape sequence
         if (word_count >= title_text.length){
             typingTimeout = null;
-            console.log("Inside");
-            const hr = document.querySelector("hr");
-            // Creating the custom event
-            const Fade = new CustomEvent("afterType", {});
-            // calling the event
-            hr.dispatchEvent(Fade);
             return;
         }
         
